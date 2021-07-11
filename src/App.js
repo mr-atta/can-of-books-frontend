9e538d157722049943fdf796fa2c73e42c2ee20b
mr-atta <mohammadatta97@gmail.com>
2021-07-11 19:16:00 +0300

    lab 11

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
-import Footer from './Footer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import BestBooks from "./BestBooks";
+import Login from "./Login";
+import Profile from "./componants/Profile";
+import { withAuth0 } from "@auth0/auth0-react"; // with Auth0
+
+// import LogoutButton from "./componants/LogoutButton";
+// import LoginButton from "./componants/LoginButton";
 
 class App extends React.Component {
-
   render() {
-    console.log('app', this.props);
-    return(
+    // console.log("app", this.props);
+    const isAuthenticated = this.props.auth0.isAuthenticated;
+    console.log(isAuthenticated);
+    return (
       <>
         <Router>
-          <IsLoadingAndError>
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-              </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-            </Switch>
-            <Footer />
-          </IsLoadingAndError>
+          {/* <IsLoadingAndError> */}
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+              {isAuthenticated ? <BestBooks /> : <Login />}
+            </Route>
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+            <Router exact path="/profile">
+              <Profile />
+            </Router>
+          </Switch>
+          <Footer />
+          {/* </IsLoadingAndError> */}
         </Router>
       </>
     );
   }
 }
 
-export default App;
+export default withAuth0(App); // with Auth0
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import './Header.css';
+import "./Header.css";
+
+import LoginButton from "./componants/LoginButton";
+import LogoutButton from "./componants/LogoutButton";
+import { withAuth0 } from "@auth0/auth0-react"; // with Auth0
 
 class Header extends React.Component {
   render() {
-    return(
+    // console.log(this.props);
+    const isAuthenticated = this.props.auth0.isAuthenticated; //
+    console.log(isAuthenticated);
+    return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        {isAuthenticated ? <LogoutButton /> : <LoginButton />}
       </Navbar>
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header); // with Auth0
